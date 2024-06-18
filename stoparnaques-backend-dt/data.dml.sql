dee0a82f0a51eec63c95d06994437bfe86eed845
manal benallal <manelbenallal@hotmail.fr>
2024-06-18 12:54:56 +0200

    update data et schema pr le run script

--- stoparnaques-backend-dt/data.dml.sql
+++ stoparnaques-backend-dt/data.dml.sql
@@ -6,9 +6,7 @@
  * \q
  */
 
-
 DELETE FROM categories;
-DELETE FROM articles; 
 DELETE FROM users;
 DELETE FROM roles;
 
@@ -16,24 +14,15 @@ DELETE FROM roles;
 INSERT INTO categories (name)
 VALUES ('Actualites'), ('Conseils');
 
-
-INSERT INTO articles (title, subtitle, editor, description, introduction, image_url, date_of_publication,created_at, category_id) 
+INSERT INTO roles
+	(code_role, role_name)
 	VALUES
-('Ecommerce','Tuto descriptage fiabilité du site marchand','manal','Lorem ipsum','Lorem ipsuddm','assets/IMG/ampoule/conseils1.png','2022-05-19', now(),(select c.id FROM categories c where c.name = 'Actualités')),
-('Les réseaux sociaux','contrefacon','thomas','Lorem ipsum blabla', 'Lorem ipsuddm','assets/IMG/ampoule/conseils1.png','2022-03-29', now(),(select c.id FROM categories c where c.name = 'Actualités')),
-('Influenceur et justice', 'Des influenceurs dans le collimateur de la justice!','eva','Lorem ipsum balbla.', 'Lorem ipsuddm','assets/IMG/ampoule/conseils1.png','2022-03-13', now(),(select c.id FROM categories c where c.name = 'Actualités')),
-('Nos conseils à  suivre', 'Nos 10 conseils','Massy','Lorem ipsum','Lorem ipsuddm','assets/IMG/ampoule/conseils1.png','2022-01-13', now(),(select c.id FROM categories c where c.name = 'Conseils')),
-('Notre lettre de reclamation', 'Lettre marchandise non conforme','laurine','Lorem ipsum balbla','Lorem ipsuddm','assets/IMG/ampoule/conseils1.png','2022-03-13', now(),(select c.id FROM categories c where c.name = 'Conseils')),
-('Sept ans de prison pour un ex-influenceur','Detournement de fonds publics', 'lisa','Lorem ipsum balbla', 'assets/IMG/ampoule/conseils1.png','2022-03-13', now(),(select c.id FROM categories c where c.name = 'Actualités')),
-('Obtenez répératation','Lettre non livraison','maroua','Lorem ipsum','assets/IMG/ampoule/conseils1.png','2022-10-13',now(),(select c.id FROM categories c where c.name = 'Conseils'));
+    ('ROLE_ADMIN', 'ADMIN'),  ('ROLE_USER', 'USER');
+   
+   INSERT INTO users
+	(first_name, last_name, email, password, role_id, is_enabled)
+	VALUES
+    ( 'Manal', 'Benallal', 'admin@admin.com', '$2y$10$v.8CVIY09L2BFyg1XXKwYOkXjZ6gn.6DsotJdZwjooesl0nVOaKGi', (SELECT r.id FROM roles r WHERE r.code_role = 'ROLE_ADMIN'), true);
 
-	
-	/* créer l'utilisateur admin = 1 seul role admin mais ajout de plusieurs roles dans un future proche*
-	 * mot de passe est différent du token, le token est un jeton unique pr un utilisateur qui lui permet...  */
-    
-INSERT INTO roles (name) 
-	VALUES ('admin'),('user');
 
-	/* créer utilisateur admin test */
-	 
-INSERT INTO users (first_name, last_name, username, password, role_id) VALUES ('manal','benallal','admind2@domain.com', '$2a$12$9BXonP0NXZ48gUwP2Z84dOexo8DPdcWh8uXCoRRLjXHsSjM/9TfRm', (SELECT r.id FROM roles r WHERE r.name = 'user'));
+
--- stoparnaques-backend-dt/schema.ddl.sql
+++ stoparnaques-backend-dt/schema.ddl.sql
@@ -5,11 +5,10 @@
  * \q
  */
 
-DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS articles;
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS users;
-
+DROP TABLE IF EXISTS roles;
 
 CREATE TABLE categories (  
 	id SERIAL PRIMARY KEY,
@@ -32,18 +31,28 @@ CREATE TABLE articles (
     	REFERENCES categories(id)
 );
 
-CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	first_name VARCHAR(300) NOT NULL,
-	last_name VARCHAR(300) NOT NULL,
-	username varchar(255) UNIQUE NOT NULL,
-	password varchar(60) NOT NULL,
-	role_id integer, 
-CONSTRAINT fk_role_id
-  	FOREIGN KEY (role_id)
-	REFERENCES roles(id)
+CREATE TABLE roles (
+	id SERIAL,
+	code_role VARCHAR(300) UNIQUE NOT NULL,
+	role_name VARCHAR(300) UNIQUE NOT null,
+	constraint pk_role_id primary key(id)
 );
 
 
+
+
+CREATE TABLE users (
+	id SERIAL,
+	first_name VARCHAR(300) NOT NULL,
+	last_name VARCHAR(300) NOT NULL,
+	email VARCHAR(300) UNIQUE NOT NULL,
+	password VARCHAR(1000) NOT NULL,
+	role_id INTEGER NOT NULL,
+    is_enabled BOOLEAN NOT NULL,
+	constraint pk_user_id primary key(id),
+	constraint fk_role_id 
+			foreign key (role_id)
+			references roles(id)
+);
 
 
